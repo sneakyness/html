653c108ff37e7647078e681bf633dccd6ffca11e
Sam Ruby <rubys@intertwingly.net>
2012-08-16 10:57:51 -0400

    Modifications related to one-step publishing

--- README.md
+++ README.md
@@ -7,6 +7,4 @@ Deliverables of the HTML Working Group
 
 Build instructions:
 
-    ruby scripts/boilerplate.rb [2dcontext] > /tmp/html5.in
-    anolis --w3c-compat --allow-duplicate-dfns --parser=lxml.html \
-      /tmp/htm5.in /tmp/Overview.html
+    python scripts/publish.py [html|2dcontext]
--- scripts/boilerplate.py
+++ scripts/boilerplate.py
@@ -1,56 +1,65 @@
-import os, sys, re
-os.chdir(os.path.abspath(os.path.join(__file__, '../..')))
+import sys
 
-# select document
-if len(sys.argv)>1 and sys.argv[0] == '2dcontext':
-  header = open("boilerplate/header-w3c-html-2dcontext").read()
-  select = '2dcontext'
-else:
-  header = open("boilerplate/header-w3c-html-core").read()
-  select = 'w3c-html'
+def main(stdin, stdout, select='w3c-html'):
+  import os, re
+  os.chdir(os.path.abspath(os.path.join(__file__, '../..')))
 
-# remove instructions
-header = re.compile('<!-- .*? -->\n?', re.S).sub('', header)
-
-# capture variables
-vars = {}
-def vset(match): 
-  vars[match.group(1)]=match.group(2)
-  return ''
-header = re.sub('<!--SET (\w+)=(.*?)-->\n?', vset, header)
-
-# include nested boiler plate
-def boilerplate(match):
-  name = match.group(1)
-  if os.path.exists("boilerplate/%s" % name):
-    content = open("boilerplate/%s" % name).read()
+  # select document
+  if select == '2dcontext':
+    header = open("boilerplate/header-w3c-html-2dcontext").read()
   else:
-    content = match.group(0)
-    sys.stderr.write("Missing file: %s\n" % name)
+    header = open("boilerplate/header-w3c-html-core").read()
 
-  # substitute variables
-  content = re.sub('<!--INSERT (\w+)-->', lambda n: vars[n.group(1)], content)
+  # remove instructions
+  header = re.compile('<!-- .*? -->\n?', re.S).sub('', header)
 
-  # use content as the replacement
-  return content
-header = re.sub('<!--BOILERPLATE ([-\w]+)-->', boilerplate, header)
+  # capture variables
+  vars = {}
+  def vset(match): 
+    vars[match.group(1)]=match.group(2)
+    return ''
+  header = re.sub('<!--SET (\w+)=(.*?)-->\n?', vset, header)
 
-source = open('source').read()
-source = re.compile('^.*?<!--START %s-->' % select, re.DOTALL).sub('', source)
+  # include nested boiler plate
+  def boilerplate(match):
+    name = match.group(1)
+    if os.path.exists("boilerplate/%s" % name):
+      content = open("boilerplate/%s" % name).read()
+    else:
+      content = match.group(0)
+      sys.stderr.write("Missing file: %s\n" % name)
 
-content =  header + source
+    # substitute variables
+    content = re.sub('<!--INSERT (\w+)-->', lambda n: vars[n.group(1)], content)
 
-content = re.compile('<!--END %s-->.*?<!--START %s-->' % (select,select), re.DOTALL).sub('', content)
-content = re.compile('<!--END %s-->.*' % select, re.DOTALL).sub('', content)
+    # use content as the replacement
+    return content
+  header = re.sub('<!--BOILERPLATE ([-\w]+)-->', boilerplate, header)
 
-content = re.sub('<!--(START|END) [-\w]+-->\n?', '', content)
+  source = stdin.read()
+  source = re.compile('^.*?<!--START %s-->' % select, re.DOTALL).sub('', source)
 
-# select based on the value of PUB
-if vars['PUB'] == '1':
-  content = re.compile('^<!--PUB-N-->.*?\n', re.M).sub('', content)
-  content = re.compile('^<!--PUB-Y-->', re.M).sub('', content)
-else:
-  content = re.compile('^<!--PUB-Y-->.*?\n', re.M).sub('', content)
-  content = re.compile('^<!--PUB-N-->', re.M).sub('', content)
+  content =  header + source
 
-print content,
+  content = re.compile('<!--END %s-->.*?<!--START %s-->' % (select,select), re.DOTALL).sub('', content)
+  content = re.compile('<!--END %s-->.*' % select, re.DOTALL).sub('', content)
+
+  content = re.sub('<!--(START|END) [-\w]+-->\n?', '', content)
+
+  # select based on the value of PUB
+  if vars['PUB'] == '1':
+    content = re.compile('^<!--PUB-N-->.*?\n', re.M).sub('', content)
+    content = re.compile('^<!--PUB-Y-->', re.M).sub('', content)
+  else:
+    content = re.compile('^<!--PUB-Y-->.*?\n', re.M).sub('', content)
+    content = re.compile('^<!--PUB-N-->', re.M).sub('', content)
+
+  stdout.write(content)
+
+if __name__ == '__main__':
+  if len(sys.argv)>1 and sys.argv[1] == '2dcontext':
+    select = '2dcontext'
+  else:
+    select = 'w3c-html'
+
+  main(select)
