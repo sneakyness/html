56b30ef0b3bbd1c4ce9c6a0b74998e0cf6f3ac8d
Sam Ruby <rubys@intertwingly.net>
2012-08-16 11:00:42 -0400

    Apply patches from Mike Smith

--- scripts/spec-splitter.py
+++ scripts/spec-splitter.py
@@ -6,17 +6,23 @@ from copy import deepcopy
 print "HTML5 Spec Splitter"
 
 absolute_uris = False
+w3c = False
 use_html5lib_parser = False
 use_html5lib_serialiser = False
+make_index_of_terms = False
 file_args = []
 
 for arg in sys.argv[1:]:
     if arg == '--absolute':
         absolute_uris = True
+    elif arg == '--w3c':
+        w3c = True
     elif arg == '--html5lib-parser':
         use_html5lib_parser = True
     elif arg == '--html5lib-serialiser':
         use_html5lib_serialiser = True
+    elif arg == '--make-index-of-terms':
+        make_index_of_terms = True
     else:
         file_args.append(arg)
 
@@ -26,6 +32,7 @@ if len(file_args) != 2:
     print
     print 'Options:'
     print '  --absolute ............. convert relative URLs to absolute (e.g. for images)'
+    print '  --w3c .................. use W3C variant instead of WHATWG'
     print '  --html5lib-parser ...... use html5lib parser instead of lxml'
     print '  --html5lib-serialiser .. use html5lib serialiser instead of lxml'
     sys.exit()
@@ -35,24 +42,97 @@ if use_html5lib_parser or use_html5lib_serialiser:
     import html5lib.serializer
     import html5lib.treewalkers
 
-index_page = 'index'
+if w3c:
+    index_page = 'Overview'
+else:
+    index_page = 'index'
 
 # The document is split on all <h2> elements, plus the following specific elements
 # (which were chosen to split any pages that were larger than about 100-200KB, and
 # may need to be adjusted as the spec changes):
 split_exceptions = [
-    'common-microsyntaxes', 'urls', 'fetching-resources', 'common-dom-interfaces', 'namespaces', # <-- infrastructure
-    'elements', # <-- dom
+    'the-a-element', 'the-abbr-element', 'the-address-element',
+    'the-area-element', 'the-article-element', 'the-aside-element',
+    'the-audio-element', 'the-b-element', 'the-base-element',
+    'the-bdi-element', 'the-bdo-element', 'the-blockquote-element',
+    'the-body-element', 'the-br-element', 'the-button-element',
+    'the-canvas-element', 'the-caption-element', 'the-cite-element',
+    'the-code-element', 'the-col-element', 'the-colgroup-element',
+    'the-command-element', 'the-datalist-element', 'the-dd-element',
+    'the-del-element', 'the-details-element', 'the-dfn-element',
+    'the-dir-element', 'the-div-element', 'the-dl-element',
+    'the-dt-element', 'the-em-element', 'the-embed-element',
+    'the-fieldset-element', 'the-figcaption-element', 'the-figure-element',
+    'the-footer-element', 'the-form-element',
+    'the-h1-h2-h3-h4-h5-and-h6-elements', 'the-head-element',
+    'the-header-element', 'the-hgroup-element', 'the-hr-element',
+    'the-html-element', 'the-i-element', 'the-iframe-element',
+    'the-img-element', 'the-input-element', 'the-ins-element',
+    'the-kbd-element', 'the-keygen-element', 'the-label-element',
+    'the-legend-element', 'the-li-element', 'the-link-element',
+    'the-map-element', 'the-mark-element', 'the-menu-element',
+    'the-meta-element', 'the-meter-element', 'the-nav-element',
+    'the-noscript-element', 'the-object-element', 'the-ol-element',
+    'the-optgroup-element', 'the-option-element', 'the-output-element',
+    'the-p-element', 'the-param-element', 'the-pre-element',
+    'the-progress-element', 'the-q-element', 'the-rp-element',
+    'the-rt-element', 'the-ruby-element', 'the-s-element',
+    'the-samp-element', 'the-script-element', 'the-section-element',
+    'the-select-element', 'the-small-element', 'the-source-element',
+    'the-span-element', 'the-strong-element', 'the-style-element',
+    'the-sub-and-sup-elements', 'the-summary-element', 'the-table-element',
+    'the-tbody-element', 'the-td-element', 'the-textarea-element',
+    'the-tfoot-element', 'the-th-element', 'the-thead-element',
+    'the-time-element', 'the-title-element', 'the-tr-element',
+    'the-track-element', 'the-u-element', 'the-ul-element',
+    'the-var-element', 'the-video-element', 'the-wbr-element',
+
+    'styling',
+    'usage-summary',
+    'attributes-common-to-ins-and-del-elements',
+    'edits-and-paragraphs',
+    'edits-and-lists',
+    'media-elements',
+    'image-maps',
+    'mathml',
+    'svg-0',
+    'dimension-attributes',
+    'attributes-common-to-td-and-th-elements',
+    'examples',
+    'common-input-element-apis',
+
+    'global-attributes',
+    'element-definitions',
+    'common-dom-interfaces',
+    'namespaces',
+    'requirements-relating-to-bidirectional-algorithm-formatting-characters',
+    'wai-aria',
+    'interactions-with-xpath-and-xslt',
+    'headings-and-sections',
+
+    'dynamic-markup-insertion',
+    'common-microsyntaxes', 'urls', # <-- infrastructure
+    'elements', 'content-models', 'apis-in-html-documents', # <-- dom
+
+    'attributes-common-to-form-controls',
+    'textFieldSelection',
+    'constraints',
+    'form-submission',
+
+    'common-idioms-without-dedicated-elements',
 
     'scripting-1', 'sections', 'grouping-content', 'text-level-semantics', 'edits',
-    'embedded-content-1', 'the-iframe-element', 'the-video-element', 'the-canvas-element', 'the-map-element', 'tabular-data',
-    'forms', 'the-input-element', 'states-of-the-type-attribute', 'number-state', 'common-input-element-attributes', 'the-button-element', 'association-of-controls-and-forms',
-    'interactive-elements', 'commands', 'common-idioms', 'selectors', # <-- semantics
+    'embedded-content-1', 'tabular-data',
+    'forms', 'states-of-the-type-attribute', 'number-state', 'common-input-element-attributes', 'the-button-element', 'association-of-controls-and-forms',
+    'interactive-elements', 'commands', # <-- semantics
 
+    'predefined-vocabularies-0', 'converting-html-to-other-formats', # <-- microdata
     'origin-0', 'timers', 'offline', 'history', 'links', # <-- browsers
+    'user-prompts',
+    'system-state-and-capabilities',
     'dnd', # <-- editing
+    'editing-apis',
 
-    'workers', 'network', 'web-messaging', 'webstorage',
     'parsing', 'tokenization', 'tree-construction', 'the-end', 'named-character-references', # <-- syntax
 ]
 
@@ -71,6 +151,10 @@ print "Splitting..."
 
 doctitle = doc.find('.//title').text
 
+if make_index_of_terms:
+  # get all the nodes from the index of terms (if any) and save for later
+  index_of_terms = doc.xpath("//*[@class='index-of-terms']//dl")
+
 # Absolutise some references, so the spec can be hosted elsewhere
 if absolute_uris:
     for a in ('href', 'src'):
@@ -87,7 +171,8 @@ original_body = doc.find('body')
 # Create an empty body, for the page content to be added into later
 default_body = etree.Element('body')
 if original_body.get('class'): default_body.set('class', original_body.get('class'))
-if original_body.get('onload'): default_body.set('onload', 'fixBrokenLink(); %s' % original_body.get('onload'))
+default_body.set('onload', 'fixBrokenLink();')
+#if original_body.get('onload'): default_body.set('onload', 'fixBrokenLink(); %s' % original_body.get('onload'))
 original_body.getparent().replace(original_body, default_body)
 
 # Extract the header, so we can reuse it in every page
@@ -95,14 +180,14 @@ header = original_body.find('.//*[@class="head"]')
 
 # Make a stripped-down version of it
 short_header = deepcopy(header)
-del short_header[2:]
+del short_header[4:]
 
 # Extract the items in the TOC (remembering their nesting depth)
 def extract_toc_items(items, ol, depth):
     for li in ol.iterchildren():
         for c in li.iterchildren():
             if c.tag == 'a':
-                assert c.get('href')[0] == '#'
+              if c.get('href')[0] == '#':
                 items.append( (depth, c.get('href')[1:], c) )
             elif c.tag == 'ol':
                 extract_toc_items(items, c, depth+1)
@@ -110,10 +195,11 @@ toc_items = []
 extract_toc_items(toc_items, original_body.find('.//ol[@class="toc"]'), 0)
 
 # Prepare the link-fixup script
-link_fixup_script = etree.XML('<script src="link-fixup.js"/>')
-doc.find('head')[-1].tail = '\n  '
-doc.find('head').append(link_fixup_script)
-link_fixup_script.tail = '\n  '
+if not w3c:
+    link_fixup_script = etree.XML('<script src="link-fixup.js"/>')
+    doc.find('head')[-1].tail = '\n  '
+    doc.find('head').append(link_fixup_script)
+    link_fixup_script.tail = '\n  '
 
 # Stuff for fixing up references:
 
@@ -158,8 +244,8 @@ def add_class(e, cls):
 # Contents/intro page:
 
 page = deepcopy(doc)
-add_class(page.getroot(), 'split index')
 page_body = page.find('body')
+add_class(page_body, 'split index')
 
 # Keep copying stuff from the front of the source document into this
 # page, until we find the first heading that isn't class="no-toc"
@@ -172,25 +258,19 @@ pages.append( (index_page, page, 'Front cover') )
 
 # Section/subsection pages:
 
-def first_elm(e):
-    for c in e.iterchildren(tag=etree.Element):
-        return c
-    return None
-
 def should_split(e):
     if e.tag == 'h2': return True
     if e.get('id') in split_exceptions: return True
-    # handle wrapping <div>
-    if e.tag == 'div':
-        c = first_elm(e)
-        if c:
-            if c.tag == 'h2': return True
-            if c.get('id') in split_exceptions: return True
+    if e.tag == 'div' and e.get('class') == 'impl':
+        c = e.getchildren()
+        if len(c):
+            if c[0].tag == 'h2': return True
+            if c[0].get('id') in split_exceptions: return True
     return False
 
 def get_heading_text_and_id(e):
-    if e.tag == 'div':
-        node = first_elm(e)
+    if e.tag == 'div' and e.get('class') == 'impl':
+        node = e.getchildren()[0]
     else:
         node = e
     title = re.sub('\s+', ' ', etree.tostring(node, method='text').strip())
@@ -203,8 +283,8 @@ for heading in child_iter:
     print '  <%s> %s - %s' % (heading.tag, name, title)
 
     page = deepcopy(doc)
-    add_class(page.getroot(), 'split chapter')
     page_body = page.find('body')
+    add_class(page_body, 'split chapter')
 
     page.find('//title').text = title + u' \u2014 ' + doctitle
 
@@ -236,7 +316,11 @@ for i in range(len(pages)):
 
     head = doc.find('head')
 
-    nav = etree.Element('nav')
+    if w3c:
+        nav = etree.Element('div') # HTML 4 compatibility
+    else:
+        nav = etree.Element('nav')
+    nav.set('class', 'prev_next')
     nav.text = '\n   '
     nav.tail = '\n\n  '
 
@@ -253,7 +337,7 @@ for i in range(len(pages)):
     a = etree.XML('<a href="%s.html#contents">Table of contents</a>' % index_page)
     a.tail = '\n  '
     nav.append(a)
-    link = etree.XML('<link href="%s.html#contents" title="Table of contents" rel="index"/>' % index_page)
+    link = etree.XML('<link href="%s.html#contents" title="Table of contents" rel="contents"/>' % index_page)
     link.tail = '\n  '
     head.append(link)
 
@@ -300,6 +384,22 @@ for i in range(len(pages)):
 
     doc.find('body').insert(1, nav) # after the header
 
+if make_index_of_terms:
+# Write additional separate files for each term entry in the index of terms.
+# Each term entry should be a <dl> with an id attribute whose value is an id of
+# a <dfn>, with the string "_index" appended to it.
+# For now, the subdirectory for the files is hardcoded here as "index-of-terms".
+  for term in index_of_terms:
+  # the firstChild <dt> here is a name and link for the defining instance of
+  # each index term; we don't need that in this context, so just remove it
+      term.remove(term.find("./dt"))
+      fix_refs('DUMMY', term)
+      # we use the ID of the term as the base for the filename, minus the last six
+      # characters ("_index")
+      id = term.get("id")[:-6]
+      f = open('%s/%s' % ("index-of-terms", id+".html"), 'w')
+      f.write(etree.tostring(term, pretty_print=True, method="html"))
+
 report_broken_refs()
 
 print "Outputting..."
@@ -307,17 +407,18 @@ print "Outputting..."
 # Output all the pages
 for name, doc, title in pages:
     f = open('%s/%s' % (file_args[1], get_page_filename(name)), 'w')
+#    f.write("<!doctype html>\n")
     if use_html5lib_serialiser:
         tokens = html5lib.treewalkers.getTreeWalker('lxml')(doc)
         serializer = html5lib.serializer.HTMLSerializer(quote_attr_values=True, inject_meta_charset=False)
         for text in serializer.serialize(tokens, encoding='us-ascii'):
-            f.write(text)
+          f.write(text)
     else:
         f.write(etree.tostring(doc, pretty_print=False, method="html"))
 
 # Generate the script to fix broken links
 f = open('%s/fragment-links.js' % (file_args[1]), 'w')
-links = ','.join('"%s":"%s"' % (k.replace("\\", "\\\\").replace('"', '\\"'), v) for (k,v) in id_pages.items())
+links = ','.join("'%s':'%s'" % (k.replace("\\", "\\\\").replace("'", "\\'"), v) for (k,v) in id_pages.items())
 f.write('var fragment_links = { ' + re.sub(r"([^\x20-\x7f])", lambda m: "\\u%04x" % ord(m.group(1)), links) + ' };\n')
 f.write("""
 var fragid = window.location.hash.substr(1);
